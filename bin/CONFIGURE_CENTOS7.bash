8a1bd2fd41ec60e199c81337f7d559a18a967ac9
Simson Garfinkel <simsong@acm.org>
2017-07-16 18:11:27 +0000

    CONFIGURE script updates VM and installs Python3.6, numpty and other tools

---
+++ bin/CONFIGURE_CENTOS7.bash
@@ -0,0 +1,27 @@
+# Script to configure centos7 with necessary packages to create the Synthetic US Population
+# usage: sudo bash CONFIGURE_CENTOS7.bash
+#
+# these make centos easier for use. It's not necessary, and it increases bloat, but it's generally worth it.
+yum -y install emacs
+
+# Bring the system up to date. Again, not necessary, but useful:
+yum -y makecache fast
+yum -y update
+
+
+#
+# Install Python3.4 and necessary tools
+# See https://www.digitalocean.com/community/tutorials/how-to-install-python-3-and-set-up-a-local-programming-environment-on-centos-7
+sudo yum -y install yum-utils
+sudo yum -y groupinstall development
+sudo yum -y install https://centos7.iuscommunity.org/ius-release.rpm
+sudo yum -y install python36u
+sudo yum -y install python36u-pip
+
+# Note: We do not create a python36 virtual environment, but we probably should
+
+
+#
+# this brings in the necessary tools:
+
+
